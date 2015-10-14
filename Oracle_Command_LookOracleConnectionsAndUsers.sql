0c20c4315e39f1b83b50f28b1a7a11bd93ad6c2a
1529464062 <1529464062@qq.com>
2015-10-14 21:00:35 +0800

    随便写点,以备后忘

---
+++ Oracle_Command_LookOracleConnectionsAndUsers.sql
@@ -0,0 +1,11 @@
+﻿--察看Oracle的连接数
+select count(*) from v$session;
+--查看Oracle的并发连接数
+select count(*) from v$session where status='ACTIVE';
+--查看不同用户的连接数
+select username,count(username),from v$session where username is not null group by username;
+--查看所有用户
+select * from all_users;
+--查看用户或角色系统权限(直接赋值给用户或角色的系统权限)
+select * from dba_sys_privs;
+select * from user_sys_privs;
